1747b3d8a21447f00d5d88dbefbbf28ae9373bdf
namith <anim0003@student.monash.edu>
2021-05-01 20:39:25 +0530

    Added the Bushes and the Dirt class with their functionalities from the Design Rationale.

--- src/game/Bushes.java
+++ src/game/Bushes.java
@@ -1,4 +1,32 @@
 package game;
 
-public class Bushes {
+import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Bushes extends Ground {
+    /**
+     * A class that represents the bushes.
+     */
+
+    private ArrayList<Fruit> fruits = new ArrayList<>();
+
+    public Bushes() {
+        super('b');
+    }
+
+    @Override
+    public void tick(Location location) {
+        ripening();
+    }
+
+    public void ripening(){
+
+        if (new Random().nextInt(10) == 0){
+            fruits.add(new Fruit());
+        }
+    }
+
 }
--- src/game/Dirt.java
+++ src/game/Dirt.java
@@ -1,13 +1,84 @@
 package game;
 
+import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * A class that represents bare dirt.
  */
 public class Dirt extends Ground {
 
+	private GameMap map;
+	ArrayList<Character> aroundGround = new ArrayList<>();
+
 	public Dirt() {
 		super('.');
+	}
+
+	@Override
+	public void tick(Location location) {
+		boolean bushGrew = growBush(location);
+
+		if (bushGrew){
+			location.setGround(new Bushes());
+		}
+	}
+
+	private boolean growBush(Location location) {
+		int bushCount = 0;
+		int treeCount = 0;
+
+		map = location.map();
+		int x = location.x();
+		int y = location.y();
+
+
+		if (x != 0){
+			aroundGround.add(map.at(x-1, y).getGround().getDisplayChar());
+		}
+		if (x != 79){
+			aroundGround.add(map.at(x+1, y).getGround().getDisplayChar());
+		}
+		if (y != 0){
+			aroundGround.add(map.at( x, y-1).getGround().getDisplayChar());
+		}
+		if (y != 24){
+			aroundGround.add(map.at(x, y+1).getGround().getDisplayChar());
+		}
+		if (x != 0 && y != 0){
+			aroundGround.add(map.at(x-1, y-1).getGround().getDisplayChar());
+		}
+		if (x != 0 && y != 24){
+			aroundGround.add(map.at(x-1, y+1).getGround().getDisplayChar());
+		}
+		if (x != 79 && y != 0){
+			aroundGround.add(map.at(x+1, y-1).getGround().getDisplayChar());
+		}
+		if (x != 79 && y != 24){
+			aroundGround.add(map.at(x+1, y+1).getGround().getDisplayChar());
+		}
+
+		for (char tile: aroundGround){
+			if (tile == 'b'){
+				bushCount++;
+			}
+			if (tile == '+'){
+				treeCount++;
+			}
+		}
+
+		if (treeCount != 0){
+			return false;
+		}
+		else if (bushCount >= 2){
+			return new Random().nextInt(10) == 0;
+		}
+		else{
+			return new Random().nextInt(100) == 0;
+		}
 	}
 }
