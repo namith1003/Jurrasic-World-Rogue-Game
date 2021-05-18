fc0bc7c38cdb657095a22abd2db8f2b00b592108
namith <anim0003@student.monash.edu>
2021-05-18 19:41:55 +0530

    Added Fish and all the comments for the lake and fish classes

---
+++ src/game/Fish.java
@@ -0,0 +1,16 @@
+package game;
+
+/**
+ * Class for the creation of fish
+ */
+public class Fish extends FoodItem{
+    /**
+     * constructor to create the food item
+     *
+     * @param name the name of the food item
+     */
+    public Fish(String name) {
+        super(name, '`');
+        healValue = 5;
+    }
+}
--- src/game/Lake.java
+++ src/game/Lake.java
@@ -3,6 +3,9 @@ package game;
 import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -15,17 +18,23 @@ public class Lake extends Ground {
      */
     private int sips;
     /**
-     * number of fish in the lake
+     * Array List of all the fish in the lake
      */
-    private int fish;
+    private ArrayList<Fish> fish;
     /**
      * the number of turns since the last chance to rain for all the lakes, rain might happen every 10 turns
      * for all lakes
      */
     private static int rainTurns = 0;
 
+    /**
+     * number of lakes that have been created
+     */
     private static int numOfLakes = 0;
 
+    /**
+     * amount of sips to be added to all lakes
+     */
     private static int rainValue=0;
 
     /**
@@ -34,7 +43,8 @@ public class Lake extends Ground {
     public Lake() {
         super('~');
         sips = 25;
-        fish = 5;
+        List<Fish> fishList = Arrays.asList(new Fish("Fish"),new Fish("Fish"),new Fish("Fish"),new Fish("Fish"),new Fish("Fish"));
+        fish.addAll(fishList);
         numOfLakes++;
     }
 
@@ -50,13 +60,21 @@ public class Lake extends Ground {
         fishBorn();
     }
 
-
+    /**
+     * creates a fish in the lake with a 60% chance of one being born every turn
+     */
     public void fishBorn(){
-        if (new Random().nextInt(5) <= 2){
-            fish++;
+        if (fish.size() <= 25) {
+            if (new Random().nextInt(5) <= 2) {
+                fish.add(new Fish("Fish"));
+            }
         }
     }
 
+    /**
+     * makes it rain for all the lakes every 10 turns with a 20% chance of raining
+     * and the lakes being refilled with sips
+     */
     public static void rain(){
         rainTurns++;
         if (rainTurns == numOfLakes*9 + 1) {
