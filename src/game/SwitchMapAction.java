882a27756ee87df6af7b102185b621911a70a83e
Jack Loh Enze <jloh0006@student.monash.edu>
2021-05-24 01:22:39 +0800

    updated SwitchMapAction.java

--- src/game/SwitchMapAction.java
+++ src/game/SwitchMapAction.java
@@ -3,16 +3,24 @@ package game;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
 
 public class SwitchMapAction extends Action {
 
+    protected GameMap targetMap;
+    public SwitchMapAction(GameMap map2) { this.targetMap = map2; }
+
     @Override
     public String execute(Actor actor, GameMap map) {
-        return null;
+        if (map.locationOf(actor).x() == 0) {
+            map.removeActor(actor);
+            return "Player has crossed to the other map!";
+        }
+        return "not used";
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return "Player has crossed to the other map!";
+        return "Player moves north";
     }
 }
