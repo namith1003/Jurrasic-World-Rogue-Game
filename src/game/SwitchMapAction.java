61c7071c99675bc8e6b6cedd31d74256272b1c6b
Jack Loh Enze <jloh0006@student.monash.edu>
2021-05-23 23:38:08 +0800

    added base class for SwitchMapAction.java

---
+++ src/game/SwitchMapAction.java
@@ -0,0 +1,18 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class SwitchMapAction extends Action {
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return "Player has crossed to the other map!";
+    }
+}
