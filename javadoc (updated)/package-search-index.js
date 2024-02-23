eb37f0ade0d993d19b8f344fdc3f6fdcb72c32b7
Namith Nimlaka <65733592+namith1003@users.noreply.github.com>
2024-02-23 20:06:23 +0530

    Delete javadoc (updated) directory

--- javadoc (updated)/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.monash.fit2099.demo.conwayslife"},{"l":"edu.monash.fit2099.demo.mars"},{"l":"edu.monash.fit2099.engine"},{"l":"edu.monash.fit2099.interfaces"},{"l":"game"}];updateSearchResults();
